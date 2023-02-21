5c4dc3b9545875801806f93973424aa68067aa31
deljuse <musindelus@gmail.com>
2023-02-21 13:57:29 +0300

    fix tests and build

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
 import {Ports} from "./application/ports";
 import {eventsAdapter} from "./services/eventsAdapter";
 
-function buildServices(ports: Ports) {
+export function buildServices(ports: Ports) {
     return eventsAdapter(ports)
-}
-
-export {
-    buildServices
 }
