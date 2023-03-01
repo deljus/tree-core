26afa7107afeff52062042cb518fef5ad3f3b85d
deljuse <musindelus@gmail.com>
2023-03-01 16:30:31 +0300

    fix: export types domain

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import {Ports} from "./application/ports";
 import {eventsAdapter} from "./services/eventsAdapter";
 
-export * as userDomain from "./domain/user"
-export * as profileDomain from "./domain/profile"
-export * as workspaceDomain from "./domain/workspace"
-export * as treeDomain from "./domain/tree"
+export * as user from "./domain/user"
+export * as profile from "./domain/profile"
+export * as workspace from "./domain/workspace"
+export * as tree from "./domain/tree"
+
+export type { User } from './domain/user'
+export type { Profile } from './domain/profile'
+export type { Workspace } from './domain/workspace'
 
 export type {
     Ports,
