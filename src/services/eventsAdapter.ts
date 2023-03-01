531815c130507271d8ff9f1d3110d68f5112a469
deljuse <musindelus@gmail.com>
2023-03-01 16:46:52 +0300

    fix: types eventsAdapter.ts

--- src/services/eventsAdapter.ts
+++ src/services/eventsAdapter.ts
@@ -1,12 +1,12 @@
-import {EventsService, Ports} from '../application/ports'
-import { registration } from '../application/auth/registration'
-import { login } from '../application/auth/login'
-import { verifyEmailCode } from '../application/auth/verifyEmailCode'
+import {Ports} from '../application/ports'
+import {registration, RegistrationDate} from '../application/auth/registration'
+import {login, LoginData} from '../application/auth/login'
+import { verifyEmailCode, VerifyEmailCodeData } from '../application/auth/verifyEmailCode'
 
-export function eventsAdapter(ports: Ports): EventsService {
+export function eventsAdapter(ports: Ports) {
     return {
-        registration: (data) => registration(data, ports),
-        login: (data) => login(data, ports),
-        verifyEmailCode: (data) => verifyEmailCode(data, ports)
+        registration: (data: RegistrationDate) => registration(data, ports),
+        login: (data: LoginData) => login(data, ports),
+        verifyEmailCode: (data: VerifyEmailCodeData) => verifyEmailCode(data, ports)
     }
 }
